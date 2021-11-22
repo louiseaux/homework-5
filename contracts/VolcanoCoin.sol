6b348004754f384bdd0d3afdbe6d9ae68eb829c4
"Ashlyn" <"ashlyn.barry1@gmail.com">
2021-11-22 19:57:27 +0000

    Added function to create new payment record

--- contracts/VolcanoCoin.sol
+++ contracts/VolcanoCoin.sol
@@ -7,12 +7,11 @@ import "@openzeppelin/contracts@4.2.0/access/Ownable.sol";
 
 contract VolcanoCoin is ERC20, Ownable {
     
-    uint _totalSupply = 10000;
+    uint public _totalSupply;
     
     event Supply_increase(uint);
-    event Transfer(address indexed, uint);
     
-    mapping(address => Payment[]) payments;
+    mapping(address => Payment[]) public payments;
     
     struct Payment {
         address recipient;
@@ -20,20 +19,24 @@ contract VolcanoCoin is ERC20, Ownable {
     }
     
     constructor() ERC20("VolcanoCoin", "VOL") {
-        _mint(msg.sender, _totalSupply);
+        _totalSupply = 10000;
+        _mint(_msgSender(), _totalSupply);
     }
     
-    function transferTo(address _recipient, uint _amount) public {
-        require(_amount > 0, "Amount must be > 0");
-        
-        transfer(_recipient, _amount);
-        emit Transfer(_recipient, _amount);
+    function transfer(address _recipient, uint256 _amount) public override returns(bool) {
+        _transfer(_msgSender(), _recipient, _amount);
+        newPayment(_msgSender(), _recipient, _amount);
+        return true;
+    }
+    
+    function newPayment(address _sender, address _recipient, uint _amount) public {
+        require(_sender == _msgSender());
         
         Payment memory payment;
         payment.recipient = _recipient;
         payment.amount = _amount;
         
-        payments[msg.sender].push(payment);
+        payments[_msgSender()].push(payment);
     }
     
     function getPayments(address user) public view returns (Payment[] memory) {
@@ -44,6 +47,6 @@ contract VolcanoCoin is ERC20, Ownable {
         _totalSupply += _amount;
         emit Supply_increase(_amount);
         
-        _mint(msg.sender, _amount);
+        _mint(_msgSender(), _amount);
     }
 }
